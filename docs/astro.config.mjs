1763aa8c59ab0a2b598c8be8e000a6a2d6b452df
Alex Way <alexwa@softcat.com>
2023-08-30 19:30:47 +0100

    Add docs starter

---
+++ docs/astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://alex-way.github.io',
+  	base: '/swimlane-cli',
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: {
+				github: 'https://github.com/alex-way',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
